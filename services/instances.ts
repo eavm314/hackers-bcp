304ad716bc3992c5bd1b6c18a7ca6b86c8eaccf0
enrique <enriqueadhemar@gmail.com>
2024-06-29 21:07:46 -0400

    feat: crud mongo fake

---
+++ services/instances.ts
@@ -0,0 +1,12 @@
+export const baseURL = process.env.NEWS_URL; // Replace with your desired URL
+export const apiKey = process.env.NEWS_API_KEY; // Replace with your desired API key
+export const firebaseCredentials = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
